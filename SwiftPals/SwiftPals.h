dedaaa328279b0eed0af37c8c26826a4378ec080
Jyotsna <alice21aug@gmail.com>
2020-01-29 18:23:52 +0530

    Publishing SwiftPals CocoaPods

---
+++ SwiftPals/SwiftPals.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPals.h
+//  SwiftPals
+//
+//  Created by Jyotsna on 29/01/20.
+//  Copyright © 2020 Jyotsna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftPals.
+FOUNDATION_EXPORT double SwiftPalsVersionNumber;
+
+//! Project version string for SwiftPals.
+FOUNDATION_EXPORT const unsigned char SwiftPalsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPals/PublicHeader.h>
+
+
---
+++ SwiftPalsExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPalsExamples
+//
+//  Created by Jyotsna on 29/01/20.
+//  Copyright © 2020 Jyotsna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
